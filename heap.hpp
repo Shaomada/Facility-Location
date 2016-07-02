e547a9ce949b13e2806efffea386dfbcf7c76321
Shaomada <stefan.rabenstein@web.de>
2016-07-02 21:52:33 +0200

    first implementation of a Fibbonacciheap

--- heap.hpp
+++ heap.hpp
@@ -3,93 +3,163 @@
 
 #include <vector>
 
-typedef __SIZE_TYPE__ size_t;
-
 template <typename T>
 class Heap {
 public:
-  class Node {
-  public:
-    Node (T *content, size_t pos)
-      : content(content), pos(pos) {}
-    T *content;
-    size_t pos;
-  };
-  Heap () : _heap() {};
-  ~Heap () { for (Node *n : _heap) delete n; }
+  class Node;
+  Heap ();
   Node *add (T *content);
   void decrease (Node *n);
   T *extract_min();
-
-  std::vector<Node *> get_heap () {
-    return _heap;
-  }
-  unsigned size () const {
-    return _heap.size();
-  }
 private:
-  std::vector<Node *> _heap;
-  
-  bool cmp (const Node *n, const Node *m) const {
-    return *n->content < *m->content;
-  }
+  void iter(Node *n);
+  void plant(Node *n);
+  std::vector<Node *> roots;
 };
 
 template <typename T>
-typename Heap<T>::Node *Heap<T>::add (T *content) {
-  size_t i, j = _heap.size();
-  Node *n = new Node(content, j);
-  _heap.push_back(n);
-  while (i=j,j=(i-1)/2,i>0&&cmp(_heap.at(i),_heap.at(j))) {
-    std::swap(_heap.at(i), _heap.at(j));
-    std::swap(_heap.at(i)->pos, _heap.at(j)->pos);
+class Heap<T>::Node {
+  friend class Heap<T>;
+private:
+  Node (T *t);
+  void push (Node *n);
+  void pop();
+  bool operator<(const Node &other) { return *content < *other.content; }
+   
+  T *content;
+  bool phi;
+  unsigned size;
+  Node *parent;
+  Node *first;
+  Node *next;
+  Node *prev;
+};
+
+template <typename T>
+Heap<T>::Node::Node (T *t)
+: content(t)
+, phi(false)
+, size(0)
+, parent(nullptr)
+, first(nullptr)
+, next(nullptr)
+, prev(nullptr)
+{ }
+
+template <typename T>
+void Heap<T>::Node::push (Node *n)
+{
+  n->parent = this;
+  n->prev = nullptr;
+  n->next = first;
+  first = n;
+  if (n->next) {
+    n->next->prev = n;
   }
+  ++size;
+}
+
+template <typename T>
+void Heap<T>::Node::pop ()
+{
+  if (prev) {
+    prev->next = next;
+  } else {
+    parent->first = next;
+  }
+  if (next) {
+    next->prev = prev;
+  }
+  prev = nullptr;
+  next = nullptr;
+  parent->size--;
+  parent = nullptr;
+}
+
+template <typename T>
+Heap<T>::Heap() : roots() {}
+
+template <typename T>
+typename Heap<T>::Node *Heap<T>::add (T *content) {
+  Node *n = new Node(content);
+  plant(n);
   return n;
 }
 
 template <typename T>
-void Heap<T>::decrease (Node *n) {
-  if (n == nullptr) {
-    return; // TODO: find out why this happens
-  }
-  size_t i, j = n->pos;
-  while ((i = j) && cmp(_heap.at(i), _heap.at((j = (i-1)/2)))) {
-    std::swap(_heap.at(i), _heap.at(j));
-    std::swap(_heap.at(i)->pos, _heap.at(j)->pos);
+void Heap<T>::iter (Node *n)
+{
+  if (!n->parent) {
+    // n->size was decreased in previous step of iter/decrease
+    roots.at(1+n->size) = nullptr;
+    plant(n);
+  } else {
+    if (n->phi) {
+      Node *parent = n->parent;
+      n->pop();
+      iter(parent);
+      plant(n);
+    } else {
+      n->phi = true;
+    }
   }
 }
 
 template <typename T>
-T *Heap<T>::extract_min () {
-  if (_heap.size() == 0) {
-    return nullptr;
+void Heap<T>::decrease (Node *n)
+{
+  if (n->parent) {
+    Node *parent = n->parent;
+    n->pop();
+    iter(parent);
+    plant(n);
   }
-  T *minimal = _heap.front()->content;
-  delete _heap.front();
-  _heap.front() = _heap.back();
-  _heap.pop_back();
-  if (_heap.size() > 0) {
-    _heap.front()->pos = 0;
-    size_t i = 0;
-    while(true) {
-      size_t min_index = i;
-      if (2*i+1 < _heap.size() && cmp(_heap.at(2*i+1), _heap.at(min_index))) {
-        min_index = 2*i+1;
-      }
-      if (2*i+2 < _heap.size() && cmp(_heap.at(2*i+2), _heap.at(min_index))) {
-        min_index = 2*i+2;
-      }
-      if (i == min_index) {
-        break;
-      }
-      else {
-        std::swap (_heap.at(i), _heap.at(min_index));
-        std::swap (_heap.at(i)->pos, _heap.at(min_index)->pos);
-        i = min_index;
-      }
-    }
-  }
-  return minimal;
 }
 
+template <typename T>
+T *Heap<T>::extract_min()
+{
+  Node *min = nullptr;
+  for (Node *n : roots) {
+    if (n && (!min || *n < *min)) {
+      min = n;
+    }
+  }
+  if (!min) {
+    return nullptr;
+  }
+  roots.at(min->size) = nullptr;
+  Node *next;
+  for (Node *n = min->first; n != nullptr; n = next) {
+    next = n->next;
+    n->parent = nullptr;
+    n->next = nullptr;
+    n->prev = nullptr;
+    plant(n);
+  }
+  T *retval = min->content;
+  delete min;
+  return retval;
+}
+
+template <typename T>
+void Heap<T>::plant (Node *n) {
+  n->phi = false;
+  unsigned d = n->size;
+  if (roots.size() == d) {
+    roots.push_back(n);
+  } else if (!roots.at(d)) {
+    roots.at(d) = n;
+  } else {
+    Node *m = roots.at(d);
+    roots.at(d) = nullptr;
+    if (*n < *m) {
+      n->push(m);
+      plant(n);
+    } else {
+      m->push(n);
+      plant(m);
+    }
+  }
+}
 #endif
